bf00ceffa79e64f970b3b8cf23cf2d0df56d2e87
Martin Crabtree <martin.crabtree@gmail.com>
2019-10-18 13:45:01 -0400

    yay fixed seeds and routes. Endpoints /users/id   /games/id

--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -1,9 +1,9 @@
 class GameSerializer < ActiveModel::Serializer
-  attributes :id, :wealth, :assets, :surname, :popularity, :status, :effects, :user_id, :event_ids, :character_ids
+  attributes :id, :wealth, :assets, :surname, :popularity, :status, :effects, :user_id
 
-  belongs_to :user
+  # belongs_to :user
   has_many :characters
-  has_many :joingameevents
+  # has_many :joingameevents
   has_many :events, through: :joingameevents
   
 end
