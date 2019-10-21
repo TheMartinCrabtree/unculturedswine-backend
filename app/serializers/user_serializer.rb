d5511dc4046f17006a7d0601b65f85b031adee53
Martin Crabtree <martin.crabtree@gmail.com>
2019-10-21 08:19:57 -0400

    /users/id now provides an array of the games belonging to the user

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -3,5 +3,8 @@ class UserSerializer < ActiveModel::Serializer
   
   # , :password_digest
 
-  # has_many :games
+  has_many :games
+  # has_many :characters, through: :games
+  # has_many :events, through: :games
+
 end
